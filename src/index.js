62d0b01b5ec94ba21c2ebffbb2980625a1ca8e8f
Jameyel "Jay" Johnson <jameyel@everlywell.com>
2018-12-19 13:03:03 -0600

    fix stuff

--- src/index.js
+++ src/index.js
@@ -6,22 +6,7 @@ const express = require('express'),
   port = process.env.PORT || 3000;
 
 const cors = require('cors');
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With'
-  );
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-};
-app.use(cors(), allowCrossDomain());
+app.use(cors());
 
 /** Database */
 const mongoose = require('mongoose');
